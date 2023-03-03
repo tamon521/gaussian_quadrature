ffc772d9c3de8a510faf944a1c87f0824d19e3f1
末広多聞 <tamonsuehiro@tamon.local>
2023-03-03 22:22:18 +0900

    feat: [-1,1]の区間の定積分を実装

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # gaussian_quadrature
+Rustでガウス求積を行うプログラム．
+
+## ガウス求積とは
+[ガウス求積](https://ja.wikipedia.org/wiki/ガウス求積)(Gaussian quadrature)とは，
+区間[−1, 1] で定義された実数値関数の定積分値を，比較的少ない演算で精度良く求めることができるアルゴリズムである．
+このプログラムでは**ガウス・ルジャンドル公式による求積**を用いた．
+
+これらの導出については[ガウス求積法の導出](https://slpr.sakura.ne.jp/qp/guass-legendre-quadrature-derivation/)等を参照．
+
+## 実行方法
+
+```
+$ git clone https://github.com/tamon521/gaussian_quadrature.git
+$ cd gaussian_quadrature
+$ cargo run 
+```
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use std::f64::consts::PI;
 fn main() {
-    let n = 10;
+    let n = 5;
     let f = |x: f64| x.exp(); // 関数f(x)を定義
     let result = gaussian_quadrature(n, f); // 関数f(x)をn次のGaussian Quadratureで積分
-    println!("The result is {}", result);
+    println!("The result          = {}", result);
+    println!("The analysis result = {}", 1.0_f64.exp() - (-1.0_f64).exp());
 }
 
 fn gaussian_quadrature<F>(n: usize, f: F) -> f64
@@ -19,24 +20,88 @@ where
 }
 
 fn gauss_legendre(n: usize) -> (Vec<f64>, Vec<f64>) {
+    // let m = (n + 1) / 2;
     let mut x = vec![0.0; n];
     let mut w = vec![0.0; n];
-    let m = (n + 1) / 2;
-    for i in 0..m {
-        let z = ((i as f64) + 0.75) * PI / (n as f64 + 0.5);
-        let mut p = 1.0;
-        let mut pp = 0.0;
-        for _j in 0..n {
-            let tmp = p;
-            p = legendre_poly(_j, z) * (2.0 / (n as f64) + 1.0) - pp / (n as f64) + 1.0;
-            pp = tmp;
-        }
-        x[i] = -p.sqrt();
-        x[n - 1 - i] = -x[i];
-        w[i] = 2.0 / ((1.0 - x[i] * x[i]) * pp * pp);
-        w[n - 1 - i] = w[i];
+    // let eps = 1e-15;
+    let mut z;
+    let mut x0;
+    for i in 0..n {
+        x0 = ((i as f64 + 0.75) / (n as f64 + 0.5) * PI).cos();
+        z = zeropoint_legendre_poly(n, x0);
+        x[i] = z;
+        w[i] = 2.0 * (1.0 - z * z)
+            / (n as f64 * legendre_poly(n - 1, z))
+            / (n as f64 * legendre_poly(n - 1, z));
     }
     (x, w)
+}
+
+#[test]
+fn test_gauss_legendre() {
+    assert_eq!(gauss_legendre(1), (vec![0.0], vec![2.0]));
+    assert_eq!(
+        gauss_legendre(2),
+        (
+            vec![1.0_f64 / 3.0_f64.sqrt(), -1.0_f64 / 3.0_f64.sqrt()],
+            vec![1.0; 2]
+        )
+    );
+    assert_eq!(
+        gauss_legendre(3),
+        (
+            vec![0.6_f64.sqrt(), 0.0, -0.6_f64.sqrt()],
+            vec![5.0 / 9.0, 8.0 / 9.0, 5.0 / 9.0]
+        )
+    );
+    // todo ここの処理を書く + fn gauss_legendre を解読して動くようにする
+}
+
+fn zeropoint_legendre_poly(n: usize, mut x0: f64) -> f64 {
+    let imax: i32 = 200;
+    for i in 1..=imax {
+        let x1 = x0 - legendre_poly(n, x0) / diff_legendre_poly(n, x0);
+        if (x1 - x0).abs() < 1e-16 {
+            break;
+        }
+        x0 = x1;
+        if i == imax {
+            println!("zero point not found");
+        }
+    }
+    if x0.abs() < 1e-10 {
+        x0 = 0.0;
+    }
+    x0
+}
+#[test]
+fn test_zeropoint_legendre_poly() {
+    assert!((zeropoint_legendre_poly(1, 0.5) - 0.0).abs() < 1e-10);
+    assert!((zeropoint_legendre_poly(2, 0.5) - 1.0 / 3.0_f64.sqrt()).abs() < 1e-10);
+}
+
+fn diff_legendre_poly(n: usize, mut x: f64) -> f64 {
+    // let eps = 1e-10;
+    // if (x - 1.0).abs() < eps {
+    //     return (n * (n + 1)) as f64 / 2.0;
+    // };
+    // if (x + 1.0).abs() < eps {
+    //     return power(-1.0, (n + 1).into()) * (n * (n + 1)) as f64 / 2.0;
+    // };
+    if n == 0 {
+        return 0.0;
+    }
+    if (x == 1.0) || (x == -1.0) {
+        x += 0.0001_f64;
+    }
+    return (n as f64) * (legendre_poly(n - 1, x) - x * legendre_poly(n, x)) / (1.0 - x * x);
+}
+#[test]
+fn test_diff_legendre_poly() {
+    assert_eq!(diff_legendre_poly(1, 0.0), 1.0);
+    assert_eq!(diff_legendre_poly(2, 0.0), 0.0);
+    assert_eq!(diff_legendre_poly(2, 0.5), 1.5);
+    assert_eq!(diff_legendre_poly(4, 0.5), -1.5625);
 }
 
 fn legendre_poly(n: usize, x: f64) -> f64 {
@@ -55,3 +120,26 @@ fn legendre_poly(n: usize, x: f64) -> f64 {
     }
     p1
 }
+#[test]
+fn test_legendre_poly() {
+    assert_eq!(legendre_poly(1, 0.0), 0.0);
+    assert_eq!(legendre_poly(2, 0.0), -0.5);
+    assert_eq!(legendre_poly(2, 2.0), 5.5);
+    assert_eq!(legendre_poly(3, 1.0), 1.0);
+    assert_eq!(legendre_poly(10, 1.0), 1.0);
+}
+
+// fn power(n: f64, m: i16) -> f64 {
+//     match m {
+//         0 => 1.0,
+//         1 => n,
+//         _ => {
+//             let x = power(n, m / 2);
+//             if m % 2 == 0 {
+//                 x * x
+//             } else {
+//                 x * x * n
+//             }
+//         }
+//     }
+// }
